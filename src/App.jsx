9befe5926a531c528414b7836e2f714054bc38a5
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-03-24 18:58:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Description from "./components/description/Description";
 import Feedback from "./components/feedback/Feedback";
 import Notification from "./components/notification/Notification";
